c610f925c583185b6ef45e0a728344b448d3059a
syedsufiyan888 <syedsufiyan4567@gmail.com>
2024-11-23 20:28:38 +0530

    updating functionality

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let textInput = document.querySelector(".textInput")
 let addbtn = document.querySelector(".add-btn");
-let todoList = document.querySelector(".todo-List");
+let todolist = document.querySelector(".todolist");
+let arTodo = [];
 
-addbtn.addEventListener("click",updateList);
-function updateList(){
+addbtn.addEventListener("click", updateList);
+function updateList() {
     //not taking any empty spaces as add task
-    if(textInput.value.trim()===""){
-        textInput.value='';
+    if (textInput.value.trim() === "") {
+        textInput.value = '';
         return;
     }
     //creating li after removing empty spaces.
@@ -16,29 +17,42 @@ function updateList(){
     //storing inputted text in the li
     listItem.textContent = task;
     //adding child to the unordered list
-    todoList.appendChild(listItem);
+    todolist.appendChild(listItem);
     listItem.innerHTML = `<span>${task}</span><button class="dlt-btn">X</button><br>`;
+    arTodo.push(listItem.textContent);
+    console.log(arTodo);
+
     // setting the textInput value to none after adding the task
-    textInput.value='';
-    
+    textInput.value = '';
+
     let deletebtn = listItem.querySelector(".dlt-btn");
     deletebtn.addEventListener("click", deletefn);
 }
-function deletefn(event){
+function deletefn(event) {
     event.stopPropagation();
     const li = this.parentElement;
-    todoList.removeChild(li);
+    todolist.removeChild(li);
 }
-// Dark-Theme
+//task-complete
+todolist.addEventListener("click",function(event){
+    if(event.target.tagName==="LI"){
+        event.target.classList.toggle('li-text-strike')
+    };
+})
 let theme = document.querySelector(".themebtn")
-let isDark = false;
-theme.addEventListener("click",()=>{
-    if(!isDark){
-        document.body.style="background-color: rgba(0, 0, 0, 0.819);color: rgb(255, 255, 255);";
-        theme.textContent = `light theme`;
-    }else{
-        document.body.style="background-color: white;color: black;";
-        theme.textContent = `Dark theme`;
+theme.addEventListener("click", () => {
+    document.body.classList.toggle('themechange');
+    let listItems = document.querySelectorAll("ul li");
+    listItems.forEach(item => {
+        item.classList.toggle('litheme');
+    });
+    let h2 = document.querySelector('h2');
+    h2.classList.toggle('dark-theme-text');
+    if (theme.textContent === "Dark") {
+        theme.textContent = "Light";
+    } else {
+        theme.textContent = "Dark";
     }
-    isDark=!isDark;
 });
+
+
